5402ffecee025d3de7f1a0a06c166bc75b645a73
Star <137379612@qq.com>
2015-10-09 11:03:15 +0800

    remove sen_email after logined

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
     user = User.new(email: params[:email], nickname: params[:nickname], password: params[:password])
     if user.save
       set_current_user(user)
-      UserMailer.welcome_email(user).deliver_later
+      UserMailer.welcome_email(user).deliver_now
       redirect_to controller: :apis, action: :index
     else
       flash[:error] = user.errors.full_messages.to_s
